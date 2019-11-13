ce2f3fbc6664b395fe8e2ae86ca06c07c87da5b6
Péter Sebők <33090588+sebokpeter@users.noreply.github.com>
2019-11-13 18:21:22 +0100

    Added a simple unit test
    Just to check out Junit/Mockito

--- src/test/java/SubscriptionFactoryTests.java
+++ src/test/java/SubscriptionFactoryTests.java
@@ -1,12 +1,22 @@
 
+import BLL.OPC.Subscription.AnonymousSubscription;
+import BLL.OPC.Subscription.SubscriptionFactory;
+import BLL.OPC.Subscription.SubscriptionInterface;
 import Entity.OPCData;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 /**
  *
@@ -14,14 +24,35 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class SubscriptionFactoryTests {
     
-    private List<OPCData> anonymousOPCData;
-    private List<OPCData> usernameOPCData;
+    private static List<OPCData> anonymousOPCData;
+    private static List<OPCData> usernameOPCData;
     
+    private static final String MOCK_URL = "opc.tcp://localhost:48010";
+    private static final String MOCK_USERNAME = "TEST_NAME";
+    private static final String MOCK_PW = "Password";
+        
     public SubscriptionFactoryTests() {
+        anonymousOPCData = new ArrayList<>();
+        
+        for (int i = 1; i <= 5; i++) {
+            anonymousOPCData.add(new OPCData(i, MOCK_URL, Boolean.TRUE, ("Test_Data_Anon_" + i)));
+        }
+        
+        usernameOPCData = new ArrayList<>();
+        
+        for (int i = 0; i <= 5; i++) {
+            OPCData data = new OPCData(i, MOCK_URL, Boolean.FALSE, ("TEST_DATA_USERNM_" + i));
+            
+            data.setUsername(MOCK_USERNAME + "_" + i);
+            data.setPassword(MOCK_PW);
+            
+            usernameOPCData.add(data);      
+        }
     }
     
     @BeforeAll
     public static void setUpClass() {
+
     }
     
     @AfterAll
@@ -30,6 +61,7 @@ public class SubscriptionFactoryTests {
     
     @BeforeEach
     public void setUp() {
+
     }
     
     @AfterEach
@@ -41,4 +73,21 @@ public class SubscriptionFactoryTests {
     //
     // @Test
     // public void hello() {}
+    @Test
+    public void testAnonymous() {
+        SubscriptionFactory factory = mock(SubscriptionFactory.class);
+        try {
+            when(factory.createSubscription(any(OPCData.class))).thenCallRealMethod();
+            for (OPCData oPCData : anonymousOPCData) {
+                SubscriptionInterface result = null;
+                result = factory.createSubscription(oPCData);     
+
+                assertTrue(result instanceof AnonymousSubscription);
+        }
+        } catch (Exception e) {
+            
+        }
+        
+
+    }
 }

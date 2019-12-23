a5cc94ee204c23603263baf6b586b00e0fa3d6a2
Péter Sebők <33090588+sebokpeter@users.noreply.github.com>
2019-12-23 16:40:47 +0100

    Version update

--- src/test/java/DataAccessManagerTests.java
+++ src/test/java/DataAccessManagerTests.java
@@ -12,7 +12,6 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
